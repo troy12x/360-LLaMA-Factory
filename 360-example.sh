d70064f5e1464e4c31b780ff80ab9df4a276dc83
Eyad Gomaa <61633360+troy12x@users.noreply.github.com>
2025-03-31 04:25:35 +0200

    Update 360-example.sh

--- 360-example.sh
+++ 360-example.sh
@@ -1,70 +1,44 @@
-export DS_SKIP_CUDA_CHECK=1 
-export DISABLE_VERSION_CHECK=1  # if necessary
-# sft
-deepspeed --hostfile=hostfile.2nodes src/train.py \
+# Light-R1 SFT used a slightly different internal version codebase. This script is the closest counterpart in 360-LLaMA-Factory
+# Light-R1 DPO used 360-LLaMA-Factory directly
+deepspeed --hostfile $hostfile src/train.py \
     --stage sft \
     --do_train \
-    --model_name_or_path xxx \
-    --dataset sft_toy \
+    --max_steps -1 \
+    --model_name_or_path deepseek-ai/DeepSeek-R1-Distill-Qwen-7B \
     --template qwen \
+    --dataset quasar \
+    --preprocessing_num_workers 16 \
     --finetuning_type full \
-    --output_dir output/sft-test \
+    --sequence_parallel_size 1 \
+    --gradient_checkpointing True \
+    --flash_attn fa2  \
     --cache_dir .cache \
     --overwrite_cache \
-    --overwrite_output_dir \
-    --cutoff_len 100000 \
-    --drop_exceed_length_data True \
+    --cutoff_len 20000 \
+    --output_dir quasar/model/ \
     --per_device_train_batch_size 1 \
     --gradient_accumulation_steps 1 \
     --lr_scheduler_type cosine \
-    --warmup_ratio 0.0 \
+    --save_strategy epoch \
     --logging_steps 1 \
-    --save_strategy steps \
-    --save_steps 2000 \
-    --learning_rate 5e-6 \
-    --num_train_epochs 4 \
-    --plot_loss \
+    --adam_beta1 0.9 \
+    --adam_beta2 0.95 \
+    --adam_epsilon 1e-8 \
+    --max_grad_norm 1.0 \
+    --weight_decay 0.1 \
+    --warmup_ratio 0.01 \
+    --save_total_limit 10 \
+    --learning_rate 1e-5 \
     --save_only_model True \
-    --deepspeed examples/deepspeed/ds_z3_offload_config.json \
-    --bf16 True \
-    --flash_attn fa2 \
-    --gradient_checkpointing True \
+    --num_train_epochs 3 \
+    --bf16 true \
+    --plot_loss \
     --seed 42 \
-    --sequence_parallel_size 8 \
-    --packing True \
-    --preprocessing_num_workers 32 \
-    --report_to tensorboard
-
-# dpo
-deepspeed --hostfile=hostfile.1mac src/train.py \
-    --stage dpo \
-    --do_train \
-    --model_name_or_path xxx \
-    --dataset dpo_toy \
-    --template qwen \
-    --finetuning_type full \
-    --pref_beta 0.1 \
-    --pref_loss sigmoid \
-    --output_dir output/debug \
-    --cache_dir .cache \
-    --overwrite_cache \
+    --do_eval false \
+    --deepspeed ./examples/deepspeed/ds_z3_offload_config.json \
+    --report_to tensorboard \
     --overwrite_output_dir \
-    --cutoff_len 32768 \
-    --per_device_train_batch_size 1 \
-    --gradient_accumulation_steps 4 \
-    --lr_scheduler_type cosine \
-    --warmup_ratio 0.0 \
-    --logging_steps 1 \
-    --save_steps 2000 \
-    --save_strategy steps \
-    --learning_rate 1e-6 \
-    --num_train_epochs 10 \
-    --plot_loss \
-    --save_only_model True \
-    --deepspeed examples/deepspeed/ds_z3_offload_config.json \
-    --flash_attn fa2 \
-    --gradient_checkpointing True \
-    --bf16 True \
     --ddp_timeout 180000000 \
-    --seed 42 \
-    --sequence_parallel_size 4
+    --enable_liger_kernel
+# num_train_epochs 100, with simultaneous model selection
+# stage2 Light-R1 chose epoch 8
